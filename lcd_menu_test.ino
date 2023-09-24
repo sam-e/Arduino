eda0e963d31de9936f8878303bf0f2a31c9317a8
sam-e <sam.contarino@proton.me>
2023-09-24 10:40:17 +1000

    modified:   lcd_menu_test.ino

--- lcd_menu_test.ino
+++ lcd_menu_test.ino
@@ -1,22 +1,41 @@
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
+
+//--LCD dispay
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-//Input & Button Logic
+//--Input & Button Logic
 const int numOfInputs = 5;
 //const int inputPins[numOfInputs] = {8,9,10,11,11};
 int inputState[numOfInputs];
-int lastInputState[numOfInputs] = {LOW,LOW,LOW,LOW,LOW};
-bool inputFlags[numOfInputs] = {LOW,LOW,LOW,LOW,LOW};
-long lastDebounceTime[numOfInputs] = {0,0,0,0,0};
+int lastInputState[numOfInputs] = { LOW,LOW,LOW,LOW,LOW };
+bool inputFlags[numOfInputs] = { LOW,LOW,LOW,LOW,LOW };
+long lastDebounceTime[numOfInputs] = { 0,0,0,0,0 };
 long debounceDelay = 5;
 
-//LCD Menu Logic
-const int numOfScreens = 4;
+//--LCD Menu Logic
+const int numOfScreens = 5;
 int currentScreen = 0;
-String screens[numOfScreens][2] = {{"End Voltage:","Volts"}, {"Dis. Current:", "Amps"}, 
-{"Timer:","Mins"},{"Overload Temp:","degC"}};
+String screens[numOfScreens][2] = 
+  {{"End Voltage:", "Volts"  }, 
+   {"Dis. Current:", "Amps"  }, 
+   {"Timer:", "Mins"         },
+   {"Overload Temp:", "degC" },
+   {"Save Paramaters:", "" }};
+
+//--Device parameters
 float parameters[numOfScreens];
-float parameterCheck[numOfScreens][3] = {{3.7, 4.2, 0.01}, {1.0, 10.0, 0.5}, {0.0, 120.0, 1.0}, {0.0, 65.0, 1.0}};
+float parameterCheck[numOfScreens][3] = 
+  {{ 3.7, 4.2, 0.01  },  // end discharge 
+   { 1.0, 10.0, 0.5  },  // discharge amps 
+   { 0.0, 120.0, 1.0 },  // Timer
+   { 0.0, 65.0, 1.0  }};  // Temperature 
+   
+//--EEPROM
+int eeAddress[numOfScreens];// = { 1, 32, 63, 95 };
+bool eepromFlag = 1;
+//int eeFlagAddress = 0;
+
 
 void setup() {
   //for(int i = 0; i < numOfInputs; i++) {
@@ -24,11 +43,44 @@ void setup() {
   //  digitalWrite(inputPins[i], HIGH); // pull-up 20k
   //}
   Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+  
+  for (int i = 0; i <= numOfScreens; i++) {
+    int n = 32;
+    if (i == 0) { eeAddress[i] = 1; }
+    else { eeAddress[i] = i * n; }
+  } 
+
+  //EEPROM.put(eeAddress, eeprom);
+  if (!eepromFlag) {
+    parameters[0] = 3.80;
+    parameters[1] = 5.0;
+    parameters[2] = 10.0;
+    parameters[3] = 50.0;
+
+    for (int i = 0; i <= numOfScreens; i++) {
+      EEPROM.put(eeAddress[i], parameters[i]);
+    }
+  }
+
+  for (int i =0; i < numOfScreens; i++) {
+    float f;
+    Serial.print("EEPROM Address ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(EEPROM.get(eeAddress[i], f));
+  }
+
+  //--get parameters from EEPROM
+  for (int i =0; i < numOfScreens; i++) {
+    float f;
+    parameters[i] = EEPROM.get(eeAddress[i], f);
+  }
+
   lcd.begin(16, 2);
-  parameters[0] = 3.80;
-  parameters[1] = 5.0;
-  parameters[2] = 10.0;
-  parameters[3] = 50.0;
   printScreen();
 }
 
@@ -38,23 +90,22 @@ void loop() {
 }
 
 int keystate() {     //read key value
-  int a=analogRead(0);
-  if (a > 1000) { return 100; } //no key
-  if (a > 800)  { return 5; } //select
-  if (a > 600)  { return 0; } //left
-  if (a > 400)  { return 3; } //down
-  if (a > 200)  { return 2; } //up
-  return 1;                   //right
+  int val = analogRead(A0);
+  if (val > 1000) { return 100;    } //no key
+  else if (val > 800)  { return 5; } //select
+  else if (val > 600)  { return 0; } //left
+  else if (val > 400)  { return 3; } //down
+  else if (val > 200)  { return 2; } //up
+  else { return 1; }                 //right
 }
 
 void setInputFlags() {
   int key = keystate();
-  Serial.println(key);
+  //if (debug) { Serial.println(key); }
 
   for(int i = 0; i < numOfInputs; i++) {
-    //int key = keystate();
     int reading;
-    if (key != 100 && key == i) { reading = HIGH; } //Serial.println("here");}
+    if (key != 100 && key == i) { reading = HIGH; } 
     else { reading = LOW; }
     if (reading != lastInputState[i]) {
       lastDebounceTime[i] = millis();
@@ -82,42 +133,47 @@ void resolveInputFlags() {
 }
 
 void inputAction(int input) {
+  //Serial.println(currentScreen);
+  //if (currentScreen == 3) {
+  //  Serial.println(currentScreen);
+  //  if(input == 2) { lcd.setCursor(0,1); lcd.print("No"); } 
+  //  else if(input == 3) { lcd.setCursor(0,1); lcd.print("Yes"); }
+  //}
+
   if(input == 0) {
-    if (currentScreen == 0) {
-      currentScreen = numOfScreens-1;
-    }else{
-      currentScreen--;
-    }
-  }else if(input == 1) {
-    if (currentScreen == numOfScreens-1) {
-      currentScreen = 0;
-    }else{
-      currentScreen++;
-    }
-  }else if(input == 2) {
-    parameterChange(0);
-  }else if(input == 3) {
-    parameterChange(1);
-  }
+    if (currentScreen == 0) { currentScreen = 0; } 
+    else { currentScreen--; }
+  } 
+  else if(input == 1) {
+    if (currentScreen == numOfScreens-1) { currentScreen = numOfScreens-1; } 
+    else { currentScreen++; }
+  } 
+  else if(input == 2) { parameterChange(0); } 
+  else if(input == 3) { parameterChange(1); }
 }
 
 void parameterChange(int key) {
   if(key == 0) {
-      if (parameters[currentScreen] < parameterCheck[currentScreen][1]) { 
-        parameters[currentScreen] = parameters[currentScreen] + parameterCheck[currentScreen][2]; 
-      }
-  }else if(key == 1) {
-      if (parameters[currentScreen] > parameterCheck[currentScreen][0]) { 
-        parameters[currentScreen] = parameters[currentScreen]- parameterCheck[currentScreen][2];
-      }
+    if (parameters[currentScreen] < parameterCheck[currentScreen][1]) { 
+      parameters[currentScreen] = parameters[currentScreen] + parameterCheck[currentScreen][2]; 
+    }
+  }
+  else if(key == 1) {
+    if (parameters[currentScreen] > parameterCheck[currentScreen][0]) { 
+      parameters[currentScreen] = parameters[currentScreen]- parameterCheck[currentScreen][2];
+    }
   }
 }
+
+void saveEeprom(int key){ ; }
 
 void printScreen() {
   lcd.clear();
   lcd.print(screens[currentScreen][0]);
   lcd.setCursor(0,1);
-  lcd.print(parameters[currentScreen]);
-  lcd.print(" ");
-  lcd.print(screens[currentScreen][1]);
+  if (currentScreen == numOfScreens-1) { lcd.print("No"); }
+  else { lcd.print(parameters[currentScreen]);
+    lcd.print(" ");
+    lcd.print(screens[currentScreen][1]);
+  }
 }
